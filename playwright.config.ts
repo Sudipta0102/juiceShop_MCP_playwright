d8e81491ed6694e8535033b6851366c29b94cf51
Sudipto Das <38055866+Sudipta0102@users.noreply.github.com>
2026-06-15 21:45:56 +0530

    test(config): configure Playwright startup and teardown workflow
    - added globalSetup to handle startup of AUT
    - added globalTeardown to handle teardown of AUT
    - added baseurl

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,9 +24,14 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  globalSetup: './infra/setup/global.setup.ts',
+  /* This function will run before playwright test starts to start the container. */
+   globalTeardown: './infra/setup/global.teardown.ts',
+  /* This function will stop the container after after every test */ 
+
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:8888',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -44,30 +49,6 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
