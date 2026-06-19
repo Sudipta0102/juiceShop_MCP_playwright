6616e1750797f71219bcec79e80a8845f9ca979b
Sudipto Das <38055866+Sudipta0102@users.noreply.github.com>
2026-06-19 21:36:54 +0530

    infra: modified the singleton implementation to exportable function

--- infra/db/sqliteClient.ts
+++ infra/db/sqliteClient.ts
@@ -2,15 +2,15 @@ import Database from 'better-sqlite3';
 
 const DATABASE_PATH = './container-data/juiceshop.sqlite';
 
-export const db = new Database(
-  DATABASE_PATH,
-  {
-    readonly: true
-  }
-); 
-
-// export function getDb(): Database.Database {
-//   return new Database(DATABASE_PATH, {
+// export const db = new Database(
+//   DATABASE_PATH,
+//   {
 //     readonly: true
-//   });
-// }
+//   }
+// ); 
+
+export function createDatabaseConnection(): Database.Database {
+  return new Database(DATABASE_PATH, {
+    readonly: true
+  });
+}
